555b404d6c0dbc105f5d6f8b2e87e154e2c011ac
Leo Jääskeläinen <a1600576@myy.haaga-helia.fi>
2017-04-12 14:04:49 +0000

    Add update

--- testi/boot.sh
+++ testi/boot.sh
@@ -2,5 +2,5 @@ setxkbmap fi
 sudo apt-get update
 sudo apt-get -y install puppet git
 git clone https://github.com/leksenderi/Palvelinten-hallinta-2017.git
-cd Palvelinten-hallinta-2017
+cd Palvelinten-hallinta-2017/testi
 bash boot.sh
