439c98eb4edb77c935759cbdf05d292771e18b41
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-11 19:22:42 +0000

    Update init.pp

--- Digimodule/testfolder/init.pp
+++ Digimodule/testfolder/init.pp
@@ -1,15 +1,20 @@
 class digimoduuli {
-	
-	exec { 'add-apt-repository -y ppa:webupd8team/brackets' }
-	exec { 'apt-update':
-		command => '/usr/bin/apt-get update'}
-	package { apache2:
-		require => Exec['apt-update'],
-		ensure => 'installed',
-		allowcdrom => 'true',}
-	package { brackets }
 
-	file {'/var/www/html/index.html':
-		content => "testing testing",
-	}
+        exec { 'add-apt-repository -y ppa:webupd8team/brackets':
+                command => '/usr/bin/apt-add-repository'}
+        exec { 'apt-update':
+                command => '/usr/bin/apt-get update'}
+        package { brackets
+                require => Exec['add-apt-repository -y ppa:webupd8team/brackets'],
+                require => Exec['apt-update'],
+                ensure => 'installed',
+                allowcdrom => 'true',}
+        package { apache2:
+                require => Exec['apt-update'],
+                ensure => 'installed',
+                allowcdrom => 'true',}
+
+        file {'/var/www/html/index.html':
+                content => "testing testing",
+        }
 }
