f0301583344c0945c6b57b2b3fdc2760d5f16810
leksenderi <a1600576@myy.haaga-helia.fi>
2017-05-11 21:35:16 +0000

    Updated Puppet module

--- Digimodule/testfolder/init.pp
+++ Digimodule/testfolder/init.pp
@@ -1,11 +1,8 @@
 class digimoduuli {
 
-        exec { 'add-apt-repository -y ppa:webupd8team/brackets':
-                command => '/usr/bin/apt-add-repository'}
         exec { 'apt-update':
                 command => '/usr/bin/apt-get update'}
         package { brackets
-                require => Exec['add-apt-repository -y ppa:webupd8team/brackets'],
                 require => Exec['apt-update'],
                 ensure => 'installed',
                 allowcdrom => 'true',}
